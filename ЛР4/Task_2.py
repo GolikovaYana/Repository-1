ef12b3b4d1b264b16bfae70b258160833edbc69f
GolikovaYana <148859506+GolikovaYana@users.noreply.github.com>
2023-11-25 11:23:36 +0300

    Update Task_2.py

--- ЛР4/Task_2.py
+++ ЛР4/Task_2.py
@@ -1,24 +1,20 @@
 # TODO импортировать необходимые молули
 import csv
 import json
-import os.path
 
 INPUT_FILENAME = "input.csv"
 OUTPUT_FILENAME = "output.json"
 
-def task(path_input, path_output) -> None:
-    with open(path_input, 'rt', newline='\n') as file:
+def task(file_input, file_output) -> None:
+    with open(file_input, 'rt', newline='\n') as file:
         dict_csv = [line for line in csv.DictReader(file, delimiter=',')]
-    f = open(path_output, 'w')
+    f = open(file_output, 'w')
     f.write(json.dumps(dict_csv, indent=4))
     f.close
 
-path_input_1 = os.path.abspath(INPUT_FILENAME)
-path_output_2 = os.path.abspath(OUTPUT_FILENAME)
-
 if __name__ == '__main__':
     # Нужно для проверки
-    task(path_input_1, path_output_2)
+    task(INPUT_FILENAME, OUTPUT_FILENAME)
 
     with open(OUTPUT_FILENAME) as output_f:
         for line in output_f:
